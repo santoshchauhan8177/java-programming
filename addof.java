f94be8206a965ddd89f00510eeda875004918fd0
Santosh Chauhan <155355099+santoshchauhan8177@users.noreply.github.com>
2024-10-16 20:29:31 +0530

    add program

---
+++ addof.java
@@ -0,0 +1,18 @@
+ 
+/*public class addof {
+    public static void main(String[] args) {
+        
+    int a = 10,b = 15;
+    int c = a+b;
+    System.out.println(c);
+    }
+    
+}
+*/
+public class addof {
+    public static void main(String[] args) {
+        int a= 5, b= 20;
+        int c= a+b;
+        System.out.println(c);
+    }
+}
