b11e5c24ccc624682a6179a221e23ad1db7a1722
Santosh Chauhan <155355099+santoshchauhan8177@users.noreply.github.com>
2024-10-16 20:32:49 +0530

    User Enter the value and square tow number

---
+++ areaofsqure.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+ /*import java.util.Scanner;
+ public class areaofsqure {
+    public static void main(String[] args){
+        int a,A;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value:");
+        a = sc.nextInt();
+        A = a*a;
+    System.out.println(A);
+    }
+} */
+public class areaofsqure{
+    public static void main(String[] args) {
+        int a,A;
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the value:");
+        a =sc.nextInt();
+        A = a*a;
+        System.out.println(A);
+    }
+}
