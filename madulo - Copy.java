6451edeb823374be0f11639e5e5bf1354b19d0b8
Santosh Chauhan <155355099+santoshchauhan8177@users.noreply.github.com>
2024-12-09 20:21:36 +0530

    madulo

---
+++ madulo - Copy.java
@@ -0,0 +1,10 @@
+public class madulo{
+    public static void main(String[] args)
+    {
+        int a=7;
+        int b=2;
+        int c;
+        c=a%b;
+        System.out.println(c);
+    }
+}
