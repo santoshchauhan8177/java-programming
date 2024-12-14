fd90c6c21fef8cb7cd9be04a48f7550c9aa9f0c4
Santosh Chauhan <155355099+santoshchauhan8177@users.noreply.github.com>
2024-12-14 22:11:41 +0530

    perimeter_rectangle.java

---
+++ perimeter_rectangle.java
@@ -0,0 +1,8 @@
+public class perimeter_rectangle {
+    public static void main(String[] args){
+        int l = 10;
+        int b = 2;
+        int c = 2*l + 2*b;
+    System.out.println(c);
+    }
+}
