1d32bfc59a6122214898d61a1285093a6600ddb1
Santosh Chauhan <155355099+santoshchauhan8177@users.noreply.github.com>
2024-12-29 18:13:49 +0530

    s

---
+++ mystatic.java
@@ -0,0 +1,10 @@
+public class mystatic {
+    static void myStaticMethod(){
+        System.out.println("static Method called!");
+    }
+    public static void main(String[] args) {
+        mystatic.myStaticMethod();
+        
+    }
+    
+}
